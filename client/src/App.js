1fa67b6911e4f25831ccb670af690c0a2c9924c1
Frank Valdez <fvaldez421@gmail.com>
2018-06-01 00:02:07 -0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="jumbotron">
-          <h2>
-            This is MacDaddy
-          </h2>
-        </div>
+        <Header/>
       </div>
     );
   }
