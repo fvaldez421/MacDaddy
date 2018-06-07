4411789e185e4ff645b20946ca6f833b81a83d33
Frank Valdez <fvaldez421@gmail.com>
2018-06-07 03:40:13 -0700

    deploy test

--- client/src/pages/Welcome/Welcome.js
+++ client/src/pages/Welcome/Welcome.js
@@ -8,6 +8,7 @@ const Welcome = (props) => (
             <div className="col-md-10 mr-auto ml-auto">
                 <h3>This is the Welcome Page Content</h3>
                 <h5>Please Login to continue...</h5>
+                <h6>User should be rerouted to /home if sign in is successful</h6>
             </div>
         </div>
     </div>
