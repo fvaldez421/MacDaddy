5eef8fe17a2229da45f6381052b33aa6d4c3bfc3
Frank Valdez <fvaldez421@gmail.com>
2018-06-06 20:36:34 -0700

    whoops fixed error

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 
 // Set up promises with mongoose
 mongoose.Promise = global.Promise;
-Connect to the Mongo DB
+// Connect to the Mongo DB
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/MacDaddy"
 );
