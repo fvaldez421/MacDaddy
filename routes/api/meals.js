eba4c5628a2c0051587d7c9b5b89b735ecb6a127
Frank Valdez <fvaldez421@gmail.com>
2018-06-07 22:50:27 -0700

    testing routes

--- routes/api/meals.js
+++ routes/api/meals.js
@@ -1,19 +1,19 @@
-// const router = require("express").Router();
-// const mealsController = require("../../controllers/mealsController");
+const router = require("express").Router();
+const mealsController = require("../../controllers/mealsController");
 
-// // Matches with "/api/meals"
-// router.route("/meals")
-//     .get(mealsController.findAll)
-//     .post(mealsController.create);
+// Matches with "/api/meals"
+router.route("/meals")
+    .get(mealsController.findAll)
+    .post(mealsController.create);
 
-// // Matches with "/api/meals/user/:id"
+// Matches with "/api/meals/user/:id"
 // router.route("/meals/user/:id")
 //     .get(mealsController.findByUser);
 
-// // Matches with "/api/meals/:id"
-// router.route("/meals/:id")
-//     .get(mealsController.findById)
-//     .put(mealsController.update)
-//     .delete(mealsController.remove);
+// Matches with "/api/meals/:id"
+router.route("/meals/:id")
+    .get(mealsController.findById)
+    .put(mealsController.update)
+    .delete(mealsController.remove);
 
-// module.exports = router;
+module.exports = router;
