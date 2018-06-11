8771e8099f2704a8fc1c7019585bee1ee1097756
Frank Valdez <fvaldez421@gmail.com>
2018-06-10 23:55:43 -0700

    added components and router, need to find alternative for passing  getUser()

--- client/src/pages/UserInfo/User.js
+++ client/src/pages/UserInfo/User.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import './User.css';
-// import { Link } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import UserForm from "./../../components/uForm";
 
 class User extends Component {
     componentDidMount () {
-        this.props.getUser()
+        this.props.getUser();
+        console.log(this.props);
     }
     render() {
         return (
@@ -15,7 +16,9 @@ class User extends Component {
                         <h3>This is the User Page Content</h3>
                         <h5>{this.props.user.nickName}</h5>
                         <p>The form below should be displayed conditionally using an embedded route.</p>
-                        <UserForm />
+                        {/* <Link to={`${this.props.match.url}/form`}  className="btn btn-primary" />
+                        <Route exact path = {`${this.props.match.url}/form`} component={UserForm} />
+                        <UserForm /> */}
                     </div>
                 </div>
             </div>
