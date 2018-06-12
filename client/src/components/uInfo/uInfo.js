0fcd1d756f8932de1d1bdb75ac4618065f79be9e
Frank Valdez <fvaldez421@gmail.com>
2018-06-11 21:26:17 -0700

    user info displayed in list group

--- client/src/components/uInfo/uInfo.js
+++ client/src/components/uInfo/uInfo.js
@@ -2,24 +2,110 @@ import React from "react";
 import "./uInfo.css"
 
 const userDemo = (props) => (
-    <div className="row">
-        <div className="col-md-6">
-            <h6>Nickname: {props.userName}</h6>
-            <h6>DOB: {props.DOB}</h6>
-            <h6>location: {props.location}</h6>
-            <h6>height: {props.height}</h6>
-            <h6>weight: {props.weight}</h6>
-            <h6>diet: {props.diet}</h6>
-            <h6>hobbies: {props.hobbies}</h6>
-            <h6>occ: {props.occ}</h6>
-            <h6>certs: {props.certs}</h6>
-            <h6>fat: {props.fat}</h6>
-            <h6>carb: {props.carb}</h6>
-            <h6>prot: {props.prot}</h6>
-            <h6>sodium: {props.sodium}</h6>
-            <h6>potas: {props.potas}</h6>
-            <h6>tarCals: {props.tarCals}</h6>
-            <h6>maintCals: {props.maintCals}</h6>
+    <div className="col-md-10 mr-auto ml-auto text-left">
+        <div className="row">
+            <div className="col-md-12 stats">
+                <div className="">
+                    <ul className="list-group">
+                        <li className="keys list-group-item">
+                            <div className="row">
+                                <holder className="col-sm-6 keys">Nickname: </holder>
+                                <holder className="col-sm-6 text-right"><span>{props.userName}</span></holder>
+                            </div>
+                        </li>
+                        <li className="keys list-group-item">
+                            <div className="row">
+                                <holder className="col-sm-6 keys">DOB: </holder>
+                                <holder className="col-sm-6 text-right"><span>{props.DOB}</span></holder>
+                            </div>
+                        </li>
+                        <li className="keys list-group-item">
+                            <div className="row">
+                                <holder className="col-sm-6 keys">Location: </holder>
+                                <holder className="col-sm-6 text-right"><span>{props.location}</span></holder>
+                            </div>
+                        </li>
+                        <li className="keys list-group-item">
+                            <div className="row">
+                                <holder className="col-sm-6 keys">Height: </holder>
+                                <holder className="col-sm-6 text-right"><span>{props.height}</span></holder>
+                            </div>
+                        </li>
+                        <li className="keys list-group-item">
+                            <div className="row">
+                                <holder className="col-sm-6 keys">Weight: </holder>
+                                <holder className="col-sm-6 text-right"><span>{props.weight}</span></holder>
+                            </div>
+                        </li>
+                        <li className="keys list-group-item">
+                            <div className="row">
+                                <holder className="col-sm-6 keys">Diet: </holder>
+                                <holder className="col-sm-6 text-right"><span>{props.diet}</span></holder>
+                            </div>
+                        </li>
+                        <li className="keys list-group-item">
+                            <div className="row">
+                                <holder className="col-sm-6 keys">Hobbies: </holder>
+                                <holder className="col-sm-6 text-right"><span>{props.hobbies}</span></holder>
+                            </div>
+                        </li>
+                        <li className="keys list-group-item">
+                            <div className="row">
+                                <holder className="col-sm-6 keys">Occupation: </holder>
+                                <holder className="col-sm-6 text-right"><span>{props.occ}</span></holder>
+                            </div>
+                        </li>
+                        <li className="keys list-group-item">
+                            <div className="row">
+                                <holder className="col-sm-6 keys">Certifications: </holder>
+                                <holder className="col-sm-6 text-right"><span>{props.certs}</span></holder>
+                            </div>
+                        </li>
+                        <li className="keys list-group-item">
+                            <div className="row">
+                                <holder className="col-sm-6 keys">Fat:</holder>
+                                <holder className="col-sm-6 text-right"><span>{props.fat}</span><span className="setVals">g</span></holder>
+                            </div>
+                        </li>
+                        <li className="keys list-group-item">
+                            <div className="row">
+                                <holder className="col-sm-6 keys">Carbs:</holder>
+                                <holder className="col-sm-6 text-right"><span>{props.carb}</span><span className="setVals">g</span></holder>
+                            </div>
+                        </li>
+                        <li className="keys list-group-item">
+                            <div className="row">
+                                <holder className="col-sm-6 keys">Protein:</holder>
+                                <holder className="col-sm-6 text-right"><span>{props.prot}</span><span className="setVals">g</span></holder>
+                            </div>
+                        </li>
+                        <li className="keys list-group-item">
+                            <div className="row">
+                                <holder className="col-sm-6 keys">Sodium: </holder>
+                                <holder className="col-sm-6 text-right"><span>{props.sodium}</span><span className="setVals">g</span></holder>
+                            </div>
+                        </li>
+                        <li className="keys list-group-item">
+                            <div className="row">
+                                <holder className="col-sm-6 keys">Potassium: </holder>
+                                <holder className="col-sm-6 text-right"><span>{props.potas}</span><span className="setVals">g</span></holder>
+                            </div>
+                        </li>
+                        <li className="keys list-group-item">
+                            <div className="row">
+                                <holder className="col-sm-6 keys">Target Calories: </holder>
+                                <holder className="col-sm-6 text-right"><span>{props.tarCals}</span></holder>
+                            </div>
+                        </li>
+                        <li className="keys list-group-item">
+                            <div className="row">
+                                <holder className="col-sm-6 keys">Maintenance Calories: </holder>
+                                <holder className="col-sm-6 text-right"><span>{props.maintCals}</span></holder>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 )
