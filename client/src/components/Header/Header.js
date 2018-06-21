b4cf0c770468b42b49d1b8720c5cfb497071c441
Frank Valdez <fvaldez421@gmail.com>
2018-06-20 17:59:25 -0700

    updated header with links

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,26 +2,45 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import './Header.css';
 
+const Links = (loggedIn) => {
+    return(
+    loggedIn ?
+        (
+            <div className="row">
+                <div className="col-md-10 mr-auto ml-auto mb-3">
+                    <Link to="/home" className="btn btn-primary">Home</Link>
+                    <Link to="/meals" className="btn btn-danger">Meals</Link>
+                    <Link to="/me" className="btn btn-secondary">My Info</Link>
+                </div>
+            </div>
+        )
+        :
+        (
+            <div>Log in to see more!</div>
+        )
+    )
+}
+
 class Header extends Component {
+    state = {
+        loggedIn: false
+    }
+    componentDidMount() {
+        this.setState({ loggedIn: this.props.auth.isAuthenticated() })
+    }
     render() {
         return (
             <div className="container">
                 <div className="jumbotron">
                     <h2>This is MacDaddy (Header)</h2>
                     {
-                        this.props.auth.isAuthenticated() ?
+                        this.state.loggedIn ?
                             <div className="btn btn-primary" onClick={() => { this.props.auth.logout(); this.forceUpdate() }}>Logout</div>
                             :
                             <div className="btn btn-primary" onClick={() => this.props.auth.login()}>Login</div>
                     }
                 </div>
-                <div className="row">
-                    <div className="col-md-10 mr-auto ml-auto mb-3">
-                        <Link to="/home" className="btn btn-primary">Home</Link>
-                        <Link to="/meals" className="btn btn-danger">Meals</Link>
-                        <Link to="/me" className="btn btn-secondary">My Info</Link>
-                    </div>
-                </div>
+                {Links(this.state.loggedIn)}
             </div>
         )
     }
